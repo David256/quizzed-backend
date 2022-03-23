8899579c0b7433b490870bb9bf87baa3f4fa9ba4
David256 <wasterdavid@gmail.com>
2022-03-22 23:03:04 -0500

    Update results model

--- src/models/results.js
+++ src/models/results.js
@@ -2,10 +2,15 @@ const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
 
+const ResponseSchema = Schema({
+  questionId: { type: String, required: true },
+  response: { type: Boolean, required: true },
+});
+
 const ResultsSchema = Schema({
   quizId: { type: String, required: true },
   email: { type: String, required: true },
-  responses: { type: [Boolean], required: true },
+  responses: { type: [ResponseSchema], required: true },
 });
 
 module.exports = mongoose.models.Results || mongoose.Model('Results', ResultsSchema);
