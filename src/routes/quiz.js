3dfbc4e07a9ec46febb7e7472380bccf81949f72
David256 <wasterdavid@gmail.com>
2022-03-22 23:34:14 -0500

    Add empty routers

---
+++ src/routes/quiz.js
@@ -0,0 +1,15 @@
+const log = require('npmlog');
+const express = require('express');
+const { v4: uuid } = require('uuid');
+
+const router = express.Router();
+
+router.get('/quiz', (req, res) => {});
+
+router.post('/quiz', (req, res) => {});
+
+router.put('/quiz', (req, res) => {});
+
+router.delete('/quiz', (req, res) => {});
+
+module.exports = router;
---
+++ src/routes/results.js
@@ -0,0 +1,15 @@
+const log = require('npmlog');
+const express = require('express');
+const { v4: uuid } = require('uuid');
+
+const router = express.Router();
+
+router.get('/results', (req, res) => {});
+
+router.post('/results', (req, res) => {});
+
+router.put('/results', (req, res) => {});
+
+router.delete('/results', (req, res) => {});
+
+module.exports = router;
